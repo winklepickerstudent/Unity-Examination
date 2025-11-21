73eddb17b289fd0637501fc52eaab5808971f82e
winklepickerstudent <a24noado@student.his.se>
2025-11-21 10:08:32 +0100

    Player movement Implemented

--- Unity-Examination/Assets/Scripts/PlayerController.cs
+++ Unity-Examination/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+	[SerializeField] float speed, maxSpeed;
+	[SerializeField] Animator animator;
+	[SerializeField] Rigidbody2D rb;
+
+
+	private Vector2 moveInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Mathf.Abs(rb.linearVelocity.x) < maxSpeed)
+	{
+		rb.AddForce(transform.right * moveInput.x * speed * Time.deltaTime);
+	}
     }
+
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		Debug.Log("Gets input");
+		moveInput = context.ReadValue<Vector2>();
+	}
 }
