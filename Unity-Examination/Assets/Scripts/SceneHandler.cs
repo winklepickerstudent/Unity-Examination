a0ff010274607e4de5e58ce357bf7cf07a7fd09a
winklepickerstudent <a24noado@student.his.se>
2025-11-21 09:27:02 +0100

    story background implemented

--- Unity-Examination/Assets/Scripts/SceneHandler.cs
+++ Unity-Examination/Assets/Scripts/SceneHandler.cs
@@ -3,11 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    	void Start()
-    	{
-        	DontDestroyOnLoad(this);
-    	}
 	public void LoadScene(int ID)
 	{
 		SceneManager.LoadSceneAsync(ID);
